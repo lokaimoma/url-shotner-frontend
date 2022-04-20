4f10f2d7a5106a8db54b9cb27868e25d2d8b0070
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-20 14:34:12 +0000

    Create homepage and optmize for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-	"./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clr_primary: "#5754FE",
+      },
+    },
   },
   plugins: [],
-}
+};
