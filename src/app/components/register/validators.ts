1483ef83c83fde4515ef4da0f6d1d58f6e6228d9
lokaimoma <kelvinclark2k77@gmail.com>
2022-04-21 16:41:02 +0000

    Write confirm passwords validator

---
+++ src/app/components/register/validators.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function ConfirmedValidator(
+  controlName: string,
+  matchingControlName: string
+): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const f = control.get(controlName);
+    const matchF = control.get(matchingControlName);
+
+    return f && matchF && f.value === matchF.value
+      ? null
+      : { passwordsNotEqual: true };
+  };
+}
